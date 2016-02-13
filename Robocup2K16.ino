63561b74e5699b41685b3552c98997de10ac4cc1
Eduardo Hidalgo <eshidalgo97@gmail.com>
2016-02-13 05:59:52 -0600

    Inicio de main loops

--- Robocup2K16.ino
+++ Robocup2K16.ino
@@ -46,5 +46,9 @@ void setup()
 void loop() 
 {
   // put your main code here, to run repeatedly:
+  
+  Llenar();
+  MovernosHacia(Posibilidad(LetraCuadroActual()));
+  //String posibilidades(char);
 
 }
