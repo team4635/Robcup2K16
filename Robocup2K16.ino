057386eff4e40ff978d3b745c7e67374f6de0dfe
Eduardo Hidalgo <eshidalgo97@gmail.com>
2016-02-12 22:19:44 -0600

    Correccionsillas de Ortografia

--- Robocup2K16.ino
+++ Robocup2K16.ino
@@ -1,15 +1,15 @@
 /*
  * Name: Robocup2K16
- * Description: This is the main program for the robcup competition.
- * Our code is separated into differente files in which are define by funcionality.
+ * Description: This is the main program for the robocup competition.
+ * Our code is separated into different files which are defined by funcionality.
  * 
  * This the current configuration:
  *    1. Llenar (Updates the tridimensional map)
  *      a. HayVictima (Search in the current position if there's a victim and pulls the rescue kit)
- *    2. Posibilidades (Changes Decesions to where to go)
+ *    2. Posibilidades (Changes Decisions to where to go)
  *    3. Movernos (Moves until there's new cell)
- *      a. EstarEnRampa (Detect if there's a ramp)
- *      b. Obstaculo (Detect if there's an obstacale)
+ *      a. EstarEnRampa (Detect if the robot is on a ramp)
+ *      b. Obstaculo (Detect if there's an obstacle)
  */
 
 
